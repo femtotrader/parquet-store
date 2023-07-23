71d5652f6945cc5a9144672999307bd3e0b0587b
femtotrader <femto.trader@gmail.com>
2023-07-23 18:44:28 +0200

    WIP

--- tests/test_parquet_store.py
+++ tests/test_parquet_store.py
@@ -1,5 +1,16 @@
+from os import path
 from parquet_store import ParquetStore
+from pathlib import Path
 
 
 def test_parquet_store():
-    assert 1 == 0
+    print(Path().absolute())
+    path = Path(".")
+    ps = ParquetStore(path)
+    print(ps.libraries)
+    lib = ps.library["path.to.symbols"]
+    print(lib)
+    symbols = lib.symbols
+    print(symbols)
+    data = lib.read("SYMBOL")
+    print(data)
